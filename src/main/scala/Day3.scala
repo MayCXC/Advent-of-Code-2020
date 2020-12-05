add1c9a598b8c90917a6dcfac50d7a27bd309da1
Aaron Paterson <apaterson@protonmail.com>
2020-12-05 05:19:24 -0500

    bra

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -3,8 +3,10 @@ import scala.io.Source
 object Day3 {
     val input = Source.fromFile("./input/input3.txt").getLines.toArray
 
-    println( input
-        .zipWithIndex.count{ case (line,index) => line((index*3)%line.length).equals('#') }
+    println( input.zipWithIndex
+        .count{ case (line,index) =>
+            line((index*3)%line.length).equals('#')
+        }
     )
 
     println( Array((1,1),(1,3),(1,5),(1,7),(2,1))
