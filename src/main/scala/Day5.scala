8e8627731042be6b3322ca9b5700d88ff50e26fa
Aaron Paterson <apaterson@protonmail.com>
2020-12-05 05:44:43 -0500

    could vs should

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -2,17 +2,7 @@ import scala.io.Source
 
 object Day5 {
     val input = Source.fromFile("./input/input5.txt").getLines.toArray
-
-    def bsp(lc: Char, rc: Char, li: Int, ri: Int): String => Int = {
-        case left if left.headOption.contains(lc) => bsp(lc, rc, li, (li+ri)/2)(left.tail)
-        case right if right.headOption.contains(rc) => bsp(lc, rc, (li+ri)/2, ri)(right.tail)
-        case _ => (li+ri)/2
-    }
-
-    val ids = input.map(s => (bsp('F','B',0,128)(s) << 3) | bsp('L','R',0,8)(s.substring(7)))
+    val ids = input.map(s => Integer.parseInt(s.map(Map('F' ->'0', 'B'->'1', 'L'->'0', 'R'->'1')(_)),2))
     println(ids.max)
-
-    def range(l: Array[Int]) = l.min to l.max
-
-    range(ids).find(!ids.contains(_)).foreach(println)
+    (ids.min to ids.max).find(!ids.contains(_)).foreach(println)
 }
