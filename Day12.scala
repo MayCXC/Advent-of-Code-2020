2482f4add5240a96e027d72d56905d276600b1f4
Aaron Paterson <apaterson@protonmail.com>
2020-12-12 01:02:55 -0500

    clean

--- Day12.scala
+++ Day12.scala
@@ -3,50 +3,38 @@ import scala.math
 
 object Day12 {
     val input = Source.fromFile("./input/input12.txt").getLines().toArray
-    var dx = 0
+    var dx = 0 // part one displacement and orientation
     var dy = 0
     var ox = 1
     var oy = 0
 
-    var px = 0
+    var px = 0 // part two position and waypoint
     var py = 0
     var wx = 10
     var wy = 1
 
     for(i <- 0 until input.length)
         (input(i).head, input(i).tail.toInt) match {
-            case ('N',n) => {
-                dy += n
-                wy += n
-            }
-            case ('S',n) => {
-                dy -= n
-                wy -= n
-            }
-            case ('E',n) => {
-                dx += n
-                wx += n
-            }
-            case ('W',n) => {
-                dx -= n
-                wx -= n
-            }
+            case ('N',n) => dy += n; wy += n
+            case ('S',n) => dy -= n; wy -= n
+            case ('E',n) => dx += n; wx += n
+            case ('W',n) => dx -= n; wx -= n
             case ('F',n) => {
                 dx += ox*n
                 dy += oy*n
                 px += wx*n
                 py += wy*n
             }
-            case (r,nn) => {
-                val n = nn/90 * (r match {
+            case (r,n) => {
+                val s = math.signum(n) * (r match {
                     case 'L' => +1
                     case 'R' => -1
                 })
-                for(i <- 0 until math.abs(n)) {
-                    val nox = -math.signum(n)*oy
-                    val noy = +math.signum(n)*ox
-                    val nwx = -math.signum(n)*wy
-                    val nwy = +math.signum(n)*wx
+                for(i <- 0 until math.abs(n)/90) {
+                    val nox = -oy*s
+                    val noy = +ox*s
+                    val nwx = -wy*s
+                    val nwy = +wx*s
                     ox = nox
                     oy = noy
                     wx = nwx
@@ -55,6 +43,6 @@ object Day12 {
             }
         }
     
-    println(math.abs(dx)+math.abs(dy))
-    println(math.abs(px)+math.abs(py))
+    println(math.abs(dx) + math.abs(dy))
+    println(math.abs(px) + math.abs(py))
 }
